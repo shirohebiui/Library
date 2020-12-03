1107b352a59bcfc0318393fd84a2f4924302163c
shirohebiui <shirohebiui@gmail.com>
2020-12-03 11:43:18 +0900

    2020-12-03-12:40

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+#이 코드는 모델들을 import하고 그것들을 등록하기 위해 admin.site.register 를 호출합니다.
